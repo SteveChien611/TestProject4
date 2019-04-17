b94150ea766f5dd5a0a0aa1b30d9f3d62033c612
SteveChien611 <steve.chien.career@gmail.com>
2019-04-17 13:00:19 +0800

    Initial commit

---
+++ src/main/java/com/steve/Hello.java
@@ -0,0 +1,9 @@
+package com.steve;
+
+public class Hello {
+    public static void main(String[] args) {
+        //system.out.println("hello world")
+        Person p = new Person();
+        p.hello();
+    }
+}
