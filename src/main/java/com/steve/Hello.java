5902e0b5c0c18a6480043103eb464de9a9193b4e
SteveChien611 <steve.chien.career@gmail.com>
2019-04-17 13:34:39 +0800

    Java variable, data type

--- src/main/java/com/steve/Hello.java
+++ src/main/java/com/steve/Hello.java
@@ -5,5 +5,21 @@ public class Hello {
         //system.out.println("hello world")
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        age2.byteValue();
+        char c = 'A';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        Float weight2 = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Steve";
+
+
+
     }
+
 }
